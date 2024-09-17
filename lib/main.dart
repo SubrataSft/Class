ac1dfa4f161345ab1ce3b266ad995925aeffc012
Subrata Singha <subratachandrasingha@gmail.com>
2024-09-17 06:37:34 -0700

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,90 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/home_screen.dart';
 
 void main(){
-  runApp(Calculator());
-
+  runApp(MyApp());
 }
 
-class Calculator extends StatelessWidget {
-  const Calculator({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Calculator",
-      home: const HomeScreen(),
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
+      initialRoute: "/",
+      routes: {
+        "/":(context) => const HomeScreen(),
+        "/Setting":(context)=> const SettingScreen(),
+        "/Profile":(context)=> const ProfileScreen( ),
+      },
+    );
+  }
+}
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("home"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.pushNamed(context, "/Setting");
+            }, child:const Text("Setting",)),
+            ElevatedButton(onPressed: () {
+              Navigator.pushReplacementNamed(context, "/Profile",arguments:45);
+            }, child:const Text("Profile")),
+          ],
         ),
-        inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.green)
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.green)
-            ),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.red)
-            ),
-            errorBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.red)
-            ),
-            contentPadding: EdgeInsets.symmetric(horizontal: 12,vertical: 15)
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final args =ModalRoute.of(context)?.settings.arguments as int;
+    print(args);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.pushNamedAndRemoveUntil(context, "/", (route)=>false);
+            }, child: Text("Home")),
+            ElevatedButton(onPressed: () {}, child: Text("Setting")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Setting"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text("Profile")),
+            ElevatedButton(onPressed: () {}, child: Text("Home")),
+          ],
         ),
       ),
     );
